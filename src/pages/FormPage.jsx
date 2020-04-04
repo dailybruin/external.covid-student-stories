89d01fca19b94681441f55a1fc0f12cd394efd61
jay <jongyunjaypark@gmail.com>
2020-04-03 18:50:24 -0700

    conditional rendering on the basic questions

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -1,31 +1,120 @@
 import React from "react";
 import styled from "styled-components";
 
-const FormContainer = styled("div")`
-  height: 90vh;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
-  padding: 300px;
+const Background = styled("div")`
+  background-color: white;
 `;
+
+const basic_ques = [
+  {
+    question: "What school do you attend?",
+    type: "free response",
+    choices: 0,
+    answers: [],
+    comment: "",
+    requirement: "required",
+    id: 0
+  },
+  {
+    question: "What is your major?",
+    type: "free response",
+    choices: 0,
+    answers: [],
+    comment: "",
+    requirement: "required",
+    id: 1
+  },
+  {
+    question: "What is your school year?",
+    type: "multiple choice",
+    choices: 6,
+    answers: [
+      "high school",
+      "freshman",
+      "sophomore",
+      "junior",
+      "senior",
+      "grad student"
+    ],
+    comment: "",
+    requirement: "required",
+    id: 2
+  },
+
+  {
+    question: "What is your hometown?",
+    type: "free response",
+    choices: 0,
+    answers: [],
+    comment:
+      "The place you grew up. If your hometown is not in the USA, type in (City, Country)",
+    requirement: "required",
+    id: 3
+  },
+  {
+    question: "What is your ethnicity?",
+    type: "free response",
+    choices: 0,
+    answers: [],
+    comment: "(optional)",
+    requirement: "not required",
+    id: 4
+  },
+  {
+    question: "Have you or someone you know tested positive for COIVD-19?",
+    type: "multiple choice",
+    choices: 2,
+    answers: ["yes", "no"],
+    comment: "",
+    requirement: "required",
+    id: 5
+  },
+  {
+    question:
+      "Have has COVID-19 affected the way you feel about the following?",
+    type: "multiple choice",
+    choices: 4,
+    answers: [
+      "not worried",
+      "somewhat worried",
+      "very worried",
+      "prefer not to share"
+    ],
+    id: 6
+  }
+];
 class FormPage extends React.Component {
+  constructor(props) {
+    super(props);
+    // category: false = the basics
+    // category: true = tell us more
+    this.state = {
+      index: 0,
+      category: false
+    };
+  }
+  onAnswerClick() {
+    const { index, category } = this.state;
+    if (index < 6) {
+      this.setState({ index: index + 1 });
+    } else {
+      this.setState({ category: true });
+    }
+  }
   render() {
+    const { index, category } = this.state;
+    let button;
+    if (!category) {
+      button = <button onClick={() => this.onAnswerClick()}>NEXT</button>;
+    } else {
+      button = <button>FINISH</button>;
+    }
     return (
-      <FormContainer>
-        FORM
-        <input></input>
-        <button>button</button>
-        <input></input>
-        <button>button</button>
-        <button>button</button>
-        <button>button</button>
-        <input></input>
-        <input></input>
-        <button>button</button>
-        <button>button</button>
-        <button>button</button>
-      </FormContainer>
+      <Background>
+        <h1>{this.state.category}</h1>
+        <h2>{basic_ques[this.state.index].question}</h2>
+        {button}
+      </Background>
     );
   }
 }

e862856941bed787e2812edbd8be4d6866f53698
Amy <amyseo@ucla.edu>
2020-04-03 21:15:24 -0700

    stories page update #1

--- src/pages/StoriesPage.jsx
+++ src/pages/StoriesPage.jsx
@@ -4,32 +4,66 @@ import { css } from "emotion";
 import InfiniteScroll from "react-infinite-scroller";
 
 const StoriesContainer = styled("div")`
-  height: 90vh;
+  /* height: 90vh; */
+  height: 100%;
   width: 100%;
   display: flex;
-  background-color: lightcoral;
+  /* background-color: #636f71; */
 `;
 
 const FiltersContainer = styled("div")`
   height: 100%;
-  width: 30%;
+  width: 20%;
   display: flex;
   flex-direction: column;
-  background-color: lightblue;
+  background-color: #ffffff;
 `;
 
 const ScrollContainer = styled("div")`
   height: 100%;
-  width: 70%;
+  /* width: 80%; */
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  flex-wrap: wrap;
+  background-color: #ffffff;
 `;
 
 const StoryEntry = styled("div")`
   box-sizing: border-box;
-  border: 2px solid lightgreen;
-  margin: 5px;
-  padding: 5px;
+  margin: 20px;
+  width: 30%;
+  background-color: #f7f7f7;
+  border-radius: 5px;
+`;
+
+const StoryProfile = styled("div")`
+  box-sizing: border-box;
+  background-color: #b7c0c0;
+  padding: 5px 10px 5px 10px;
+  font-weight: 500;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+`;
+
+const StoryResponse = styled("div")`
+  padding: 15px;
+  font-family: "Avenir";
+`;
+
+const Questions = styled("div")`
+  box-sizing: border-box;
+  font-weight: 600;
+  /* background-color: #ebebeb; */
+  font-family: "Avenir";
+  font-size: 20px;
+  padding: 20px;
+  text-align: center;
+`;
+
+const QuestionAndResponsesContainer = styled("div")`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
 `;
 
 export default class StoriesPage extends React.Component {
@@ -38,33 +72,44 @@ export default class StoriesPage extends React.Component {
     return (
       <>
         <StoriesContainer>
-          <FiltersContainer></FiltersContainer>
-          <ScrollContainer ref={ref => (this.scrollParentRef = ref)}>
-            <InfiniteScroll
-              pageStart={0}
-              loadMore={() => null}
-              hasMore={true || false}
-              loader={
-                <div className="loader" key={0}>
-                  Loading ...
-                </div>
-              }
-              getScrollParent={() => this.scrollParentRef}
-            >
-              {console.log(data)}
-              {data.map(
-                row =>
-                  row.testimony != "" && (
-                    <StoryEntry>
-                      <b>
-                        {row.major} at {row.school}:
-                      </b>{" "}
-                      {row.testimony}
-                    </StoryEntry>
-                  )
-              )}
-            </InfiniteScroll>
-          </ScrollContainer>
+          <FiltersContainer>
+            this is the filters container haha
+          </FiltersContainer>
+          <QuestionAndResponsesContainer>
+            <Questions>
+              <div>How has Covid-19 affected you?</div>
+              <div>
+                What do you think your school, country, or community could have
+                done differently regarding this situation?
+              </div>
+            </Questions>
+            <ScrollContainer ref={ref => (this.scrollParentRef = ref)}>
+              <InfiniteScroll
+                pageStart={0}
+                loadMore={() => null}
+                hasMore={true || false}
+                loader={
+                  <div className="loader" key={0}>
+                    Loading ...
+                  </div>
+                }
+                getScrollParent={() => this.scrollParentRef}
+              >
+                {console.log(data)}
+                {data.map(
+                  row =>
+                    row.testimony != "" && (
+                      <StoryEntry>
+                        <StoryProfile>
+                          {row.major} at {row.school}:
+                        </StoryProfile>
+                        <StoryResponse>{row.testimony}</StoryResponse>
+                      </StoryEntry>
+                    )
+                )}
+              </InfiniteScroll>
+            </ScrollContainer>
+          </QuestionAndResponsesContainer>
         </StoriesContainer>
       </>
     );

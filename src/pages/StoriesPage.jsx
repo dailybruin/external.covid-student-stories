b21a210d33b258f9251768d521da14bcd14d021b
jay <jongyunjaypark@gmail.com>
2020-04-08 22:18:40 -0700

    share button with ICONS hehe

--- src/pages/StoriesPage.jsx
+++ src/pages/StoriesPage.jsx
@@ -26,7 +26,13 @@ import {
   RedditIcon,
   TwitterShareButton,
   TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  WeiboShareButton,
+  WeiboIcon,
 } from "react-share";
+import Dropdown from "react-bootstrap/Dropdown";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Container = styled("div")`
   width: 90%;
@@ -315,26 +321,124 @@ export default class StoriesPage extends React.Component {
                                         float: right;
                                       `}
                                     >
-                                      <FacebookShareButton
-                                        url={"https://dailybruin.com/"}
-                                        quote={"'" + row[response.column] + "'"}
-                                        hashtag={"UnifhsedStories"}
-                                      >
-                                        <FacebookIcon size={24} round={true} />
-                                      </FacebookShareButton>
-                                      <RedditShareButton
-                                        url={"https://dailybruin.com/"}
-                                        title={"Unfinished Stories: COVID"}
-                                      >
-                                        <RedditIcon size={24} round={true} />
-                                      </RedditShareButton>
-                                      <TwitterShareButton
-                                        url={"https://dailybruin.com/"}
-                                        title={"'" + row[response.column] + "'"}
-                                        hashtags={["UnfinishedStories"]}
-                                      >
-                                        <TwitterIcon size={24} round={true} />
-                                      </TwitterShareButton>
+                                      <Dropdown>
+                                        <Dropdown.Toggle
+                                          variant="secondary"
+                                          id="dropdown-basic"
+                                          size="sm"
+                                        >
+                                          Share
+                                        </Dropdown.Toggle>
+                                        <Dropdown.Menu>
+                                          <Dropdown.Item>
+                                            <FacebookShareButton
+                                              url={"https://dailybruin.com/"}
+                                              quote={
+                                                '"' + row[response.column] + '"'
+                                              }
+                                              hashtag={"UnifhsedStories"}
+                                            >
+                                              <FacebookIcon
+                                                size={24}
+                                                round={true}
+                                              />
+                                              <div
+                                                className={css`
+                                                  font-size: 18px;
+                                                  float: right;
+                                                `}
+                                              >
+                                                Facebook
+                                              </div>
+                                            </FacebookShareButton>
+                                          </Dropdown.Item>
+                                          <Dropdown.Item>
+                                            <RedditShareButton
+                                              url={"https://dailybruin.com/"}
+                                              title={
+                                                "Unfinished Stories: COVID"
+                                              }
+                                            >
+                                              <RedditIcon
+                                                size={24}
+                                                round={true}
+                                              />
+                                              <div
+                                                className={css`
+                                                  font-size: 18px;
+                                                  float: right;
+                                                `}
+                                              >
+                                                Reddit
+                                              </div>
+                                            </RedditShareButton>
+                                          </Dropdown.Item>
+                                          <Dropdown.Item>
+                                            <TwitterShareButton
+                                              url={"https://dailybruin.com/"}
+                                              title={
+                                                '"' + row[response.column] + '"'
+                                              }
+                                              hashtags={["UnfinishedStories"]}
+                                            >
+                                              <TwitterIcon
+                                                size={24}
+                                                round={true}
+                                              />
+                                              <div
+                                                className={css`
+                                                  font-size: 18px;
+                                                  float: right;
+                                                `}
+                                              >
+                                                Twitter
+                                              </div>
+                                            </TwitterShareButton>
+                                          </Dropdown.Item>
+                                          <Dropdown.Item>
+                                            <WhatsappShareButton
+                                              url={"https://dailybruin.com/"}
+                                              title={
+                                                '"' + row[response.column] + '"'
+                                              }
+                                            >
+                                              <WhatsappIcon
+                                                size={24}
+                                                round={true}
+                                              />
+                                              <div
+                                                className={css`
+                                                  font-size: 18px;
+                                                  float: right;
+                                                `}
+                                              >
+                                                Whatsapp
+                                              </div>
+                                            </WhatsappShareButton>
+                                          </Dropdown.Item>
+                                          <Dropdown.Item>
+                                            <WeiboShareButton
+                                              url={"https://dailybruin.com/"}
+                                              title={
+                                                "'" + row[response.column] + "'"
+                                              }
+                                            >
+                                              <WeiboIcon
+                                                size={24}
+                                                round={true}
+                                              />
+                                              <div
+                                                className={css`
+                                                  font-size: 18px;
+                                                  float: right;
+                                                `}
+                                              >
+                                                Weibo
+                                              </div>
+                                            </WeiboShareButton>
+                                          </Dropdown.Item>
+                                        </Dropdown.Menu>
+                                      </Dropdown>
                                     </div>
                                   </ResponseEntry>
                                 )

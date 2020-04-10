19cf3c2ab30a3d4ffa5410046243caae23cdbf10
Janis Chen <janis.mee@gmail.com>
2020-04-10 00:01:18 -0700

    parts of title

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,18 +5,47 @@ import { css } from "emotion";
 import { Link as ScrollLink, scroller } from "react-scroll";
 
 const AddStoryLink = styled(Link)`
-  font-size: 36px;
-  color: #a1afbc;
+  font-size: 18px;
+  color: white;
   font-weight: 800;
+  z-index: 9999;
+`;
+
+const TitlePage = styled("div")`
+  height: 100vh;
+  width: 100%;
+  overflow: hidden !important;
+  text-overflow: clip;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: Avenir;
 `;
 
 const TitleMessage = styled("div")`
   overflow: hidden;
-  font-size: 96px;
-  padding: 50px;
+  font-size: 70px;
   font-weight: 900;
-  color: #a1afbc;
-  margin-bottom: 150px;
+  color: white;
+  padding-top: 20px;
+`;
+
+const Caption = styled("div")`
+  overflow: hidden;
+  font-size: 18px;
+  color: white;
+`;
+
+const Coloring = styled("div")`
+  overflow: hidden;
+  background-color: black;
+  opacity: 0.5;
+  width: 800px;
+  height: 220px;
+  position: absolute;
+  text-align: center;
+  bottom: 75%;
 `;
 
 const ScrollMessage = styled("div")`
@@ -29,101 +58,31 @@ const ScrollMessage = styled("div")`
   font-size: 20px;
   padding: 50px;
   box-sizing: border-box;
-  color: white;
-  background-color: #586572;
-  width: 100%;
-`;
-
-const TitlePage = styled("div")`
-  height: 100vh;
-  width: 100%;
-  overflow: hidden !important;
-  text-overflow: clip;
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-family: Avenir;
-`;
-
-const Background = styled("div")`
-  width: 100%;
-  box-sizing: border-box;
-  overflow: hidden;
-  position: absolute;
-  padding-bottom: 500px;
-  padding-right: 50px;
-  font-size: 96px;
-  font-weight: 900;
-  color: #ecedee;
-  white-space: nowrap;
-`;
-
-const Wrapping = styled("div")`
-  width: 100%;
-  box-sizing: border-box;
-  overflow: hidden;
-  position: absolute;
-  padding-bottom: 800px;
-  padding-right: 50px;
-  font-size: 96px;
-  font-weight: 900;
-  color: #ecedee;
-  white-space: nowrap;
-  padding-right: 30px;
-`;
-
-const SameLine = styled("div")`
-  width: 100%;
-  box-sizing: border-box;
-  overflow: hidden;
-  position: absolute;
-  padding-bottom: 200px;
-  padding-right: 50px;
-  font-size: 96px;
-  font-weight: 900;
-  color: #ecedee;
-  white-space: nowrap;
-  padding-right: 30px;
-`;
-
-const Hidden = styled("a")`
-  color: rgba(0, 0, 0, 0);
+  color: black;
 `;
 
 export default class Title extends React.Component {
   render() {
     return (
       <TitlePage>
-        <Wrapping>
-          Unfinished Stories Unfinished Stories Unfinished Stories Unfinished
-          Stories Unfinished Stories
-        </Wrapping>
-        <Background>
-          Unfinished Stories Unfinished Stories Unfinished Stories Unfinished
-          Stories Unfinished Stories Unfinished Stories
-        </Background>
-        <SameLine>
-          Unfinished Stories <Hidden> Unfinished Stories </Hidden> Unfinished
-          Stories
-        </SameLine>
-        <TitleMessage>COVID STORIES</TitleMessage>
-        <AddStoryLink
-          to="/form"
-          onClick={() => {
-            scroller.scrollTo("navbar", {
-              duration: 500,
-              delay: 0,
-              smooth: "easeInOutQuint",
-            });
-          }}
-        >
-          SHARE YOUR STORY
-        </AddStoryLink>
-        <ScrollMessage>
-          scroll down to see how students have been impacted
-        </ScrollMessage>
+        <Coloring>
+          <TitleMessage>Unfinished Stories</TitleMessage>
+          <Caption>
+            students' experiences about how Covid-19 has affected their lives
+          </Caption>
+          <AddStoryLink
+            to="/form"
+            onClick={() => {
+              scroller.scrollTo("navbar", {
+                duration: 500,
+                delay: 0,
+                smooth: "easeInOutQuint",
+              });
+            }}
+          >
+            SHARE YOUR STORY
+          </AddStoryLink>
+        </Coloring>
       </TitlePage>
     );
   }

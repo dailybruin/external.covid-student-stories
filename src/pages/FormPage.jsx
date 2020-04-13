17be185a3e6eb34fe662534bea9d8b57be39f1cb
Max Wu <chengmaxwu@gmail.com>
2020-04-13 11:52:14 -0700

    nah preventdefault doesnt work

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -475,7 +475,6 @@ class FormPage extends React.Component {
     return val;
   };
   submitForm = function (e) {
-    e.preventDefault();
     this.setState({ formSubmitted: true });
     return;
   };
